328b2fb08fc9ba5c05bbfe46c7ceb3b1dea52d32
archergu <649601376@qq.com>
2024-12-06 16:16:53 +0800

    chore: update deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { join } from 'node:path'
-import { defineConfig } from 'vite'
 import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite'
 import { VitePluginDoubleshot } from 'vite-plugin-doubleshot'
 
 // https://vitejs.dev/config/
